79a874c68dbf977ff28874799bf4a685e8520626
mazouzi <sarasaramazouzi@gmail.com>
2018-08-28 15:52:01 +0200

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cineAngularApp
+ * @description
+ * # cineAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cineAngularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngTouch'
+  ]);
